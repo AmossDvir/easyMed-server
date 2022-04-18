b664b61d6295b4d4ac19a006adec4ecb62c5a06a
AmossDvir <amoss.dvir@mail.huji.ac.il>
2022-04-18 22:57:57 +0300

    configuring DB

--- server/settings.py
+++ server/settings.py
@@ -26,6 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+print(SECRET_KEY)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
